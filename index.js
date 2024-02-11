9fdc1dfbdbd9b7532719ffa0c1cf98f886403da3
ahmedkhaeld <ahmed.alsaeidy.711@gmail.com>
2024-02-11 09:38:52 +0200

    Adding a Mongo Container

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongooose = require("mongoose");
 
 const app = express();
+
+mongooose.connect("mongodb://admin:password@172.28.0.2:27017/?authSource=admin")
+.then(()=>console.log("\n Connected to mongo\n"))
+.catch((e) => console.log("\n ERROR:",e));
+
 
 app.get("/", (req, res)=> {
     res.send("<h2> Hi There ! </h2>");
